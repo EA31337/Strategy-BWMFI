09525945f6ac30c4f2f6a94703e54b5e454781ff
kenorb <kenorb@users.noreply.github.com>
2021-01-11 02:11:05 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -13,14 +13,14 @@ struct Stg_BWMFI_Params_M5 : StgParams {
   // Struct constructor.
   Stg_BWMFI_Params_M5() : StgParams(stg_bwmfi_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 26;
-    signal_open_level = 1;
+    signal_open_method = -4;
+    signal_open_filter = 0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)1;
+    signal_close_level = (float)1.0;
     price_stop_method = 0;
-    price_stop_level = 10.0;
+    price_stop_level = 1.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
